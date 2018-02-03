9d125e1373762199d4249b336c9a996896fe7e6b
Aaron Shaver <AaronShaverPDX@gmail.com>
2018-02-03 10:53:56 -0800

    Some refactoring / clean coding

--- main.py
+++ main.py
@@ -1,69 +1,73 @@
 from datetime import datetime
+from enum import Enum
 import requests
 import random
 import sys
 import time
-
-# ******************************************************************************
-# This is ugly code, just hacked together for a quick solution. No unit tests
-# or anything nice like that.
-# ******************************************************************************
+import words
 
 start_time = datetime.now()
 
-MAX_LENGTH = 15
-DELAY = 0.05
-MAX_USERNAMES = 800
+class Settings(Enum):
+    MAX_LENGTH = 15
+    MAX_USERNAMES = 800
+    PREFIXES_LISTS = [words.adjectives, words.colors]
+    SUFFIXES_LISTS = [words.nouns, words.animals]
+    DELAY = 0.05
+    USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
 
 if len(sys.argv) < 2:
-    print('put number of users you want to try for as a commandline arg')
+    print('put number of users you want to try for as a numeric commandline arg')
     sys.exit()
 
 num_usernames_to_make = int(sys.argv[1])
-if num_usernames_to_make > MAX_USERNAMES:
-    print('Google gets mad if you do too many queries. If you still  want to do this, edit the .py file and run again.')
+if num_usernames_to_make > Settings.MAX_USERNAMES.value:
+    print('Google gets mad if you do too many queries. If you still  want to do this, edit MAX_USERNAMES in the .py file and run again.')
     sys.exit()
 
-with open('colors.txt') as f:
-    colors = f.readlines()
+def print_time_taken():
+    print('Username generation took this much time: ' + str(datetime.now() - start_time))
 
-with open('animals.txt') as f:
-    animals = f.readlines()
-
-with open('adjectives.txt') as f:
-    adjectives = f.readlines()
-
-with open('nouns.txt') as f:
-    nouns = f.readlines()
-
-with open('coin_terms.txt') as f:
-    coin_terms = f.readlines()
+def is_username_unique(username):
+    url = 'https://www.google.com/search?q={}&tbs=li:1'.format(username)
+    time.sleep(Settings.DELAY.value)  # to avoid Google blacklisting us
+    headers = {
+        'User-Agent': Settings.USER_AGENT.value,
+    }
+    response = requests.get(url, headers=headers)
+    html = response.text
+    if 'detected unusual traffic' in html:
+        print('Google is not happy with the traffic we are sending; aborting')
+        write_usernames()
+        print_time_taken()
+        sys.exit()
+    return 'did not match any documents' in response.text
 
 def write_usernames():
-    usernames_sorted = sorted(usernames_final, key=len)  # shortest first
+    usernames_sorted = sorted(unique_usernames, key=len)  # shortest first
     print("\nFound {} unique and short usernames\n".format(len(usernames_sorted)))
 
     with open('results.txt', 'w') as f:
         for username in usernames_sorted:
             f.write(username + '\n')
 
-    print('Username generation took this much time: ' +
-        str(datetime.now() - start_time))
-
 def get_username():
-    for _ in range(10):  # in case username too long and have to try again
-        prefix_list_item = random.choice([coin_terms])
+    for _ in range(10):  # in case username too long and have to try again, kind of a hack
+        prefix_list_item = random.choice(Settings.PREFIXES_LISTS.value)
         prefix = random.choice(prefix_list_item)
 
-        suffix_list_item = random.choice([coin_terms])
+        suffix_list_item = random.choice(Settings.SUFFIXES_LISTS.value)
         suffix = random.choice(suffix_list_item)
         if suffix == prefix:
-            continue
+            continue  # don't want repeats
 
         username = prefix.title() + suffix.title()
         username = ''.join(c for c in username if c.isalnum())  # strip special
-        if len(username) <= MAX_LENGTH:
+        if len(username) <= Settings.MAX_LENGTH.value:
             return username
+
+def print_num_users_checked(count):
+    print('Checked {} usernames'.format(count))
 
 usernames = []
 for _ in range(num_usernames_to_make):
@@ -71,28 +75,20 @@ for _ in range(num_usernames_to_make):
     if username not in usernames:
         usernames.append(username)
 
-def is_username_unique(username):
-    url = 'https://www.google.com/search?q={}&tbs=li:1'.format(username)
-    time.sleep(DELAY)  # to avoid Google blacklisting us
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
-    }
-    response = requests.get(url, headers=headers)
-    html = response.text
-    if 'detected unusual traffic' in html:
-        print('Google is not happy with the traffic we are sending; abort')
-        write_usernames()
-        sys.exit()
-    return 'did not match any documents' in response.text
-
 num_users = len(usernames)
-print('\nChecking [ {} ] usernames'.format(num_users))
+print('\nChecking [ {} ] usernames\n'.format(num_users))
 
-usernames_final = []
+unique_usernames = []
 count = 0
 for username in usernames:
     if is_username_unique(username):
-        usernames_final.append(username)
+        unique_usernames.append(username)
     count += 1
     if count % 100 == 0:
-        print('Checked {} usernames'.format(count))
+        print_num_users_checked(count)
+
+write_usernames()
+
+print_num_users_checked(count)
+print('{} usernames were found to be unique'.format(len(unique_usernames)))
+print_time_taken()
---
+++ words.py
@@ -0,0 +1,14 @@
+with open('colors.txt') as f:
+    colors = f.readlines()
+
+with open('animals.txt') as f:
+    animals = f.readlines()
+
+with open('adjectives.txt') as f:
+    adjectives = f.readlines()
+
+with open('nouns.txt') as f:
+    nouns = f.readlines()
+
+with open('coin_terms.txt') as f:
+    coin_terms = f.readlines()
